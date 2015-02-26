21af5517356fe3ac88e709b85032fdade9b6275c
Joe Alcorn <joealcorn123@gmail.com>
2015-02-26 20:44:11 +0000

    Add local_settings.py file

--- berth/settings/dev.py
+++ berth/settings/dev.py
@@ -2,3 +2,8 @@ from berth.settings.common import *
 
 DEBUG = True
 TEMPLATE_DEBUG = True
+
+try:
+    from berth.settings.local_settings import *
+except ImportError:
+    pass
---
+++ berth/settings/local_settings.py
@@ -0,0 +1,3 @@
+# Any changes to settings for your local dev environment only
+# should be made in here and left out of the repo. Things
+# that will apply to all development environments belong in dev.py
